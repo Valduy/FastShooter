6711b5c6df75bacb70213fa79f0c0e1ced5e74ea
Valduy <whnpir@gmail.com>
2022-09-18 20:41:20 +0300

    Implement lose logic when engines destroyed

--- Assets/Game/Scripts/TrainGameManager.cs
+++ Assets/Game/Scripts/TrainGameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrainGameManager : MonoBehaviour
 {
     private float _currentDistance;
+    private bool _isEndFlag;
 
     public string GameWinScene;
     public float Speed;
@@ -14,20 +15,63 @@ public class TrainGameManager : MonoBehaviour
     public void Start()
     {
         _currentDistance = 0;
+        _isEndFlag = false;
     }
 
     private void FixedUpdate()
     {
+        if (_isEndFlag) 
+        {
+            return;
+        }
+
         var multiplier = Engines
             .Select(h => h.CurrentHealth / h.MaximumHealth)
             .Sum();
         
+        if (multiplier <= 0)
+        {
+            OnLose();
+            _isEndFlag = true;
+        }
+
         _currentDistance += multiplier * Speed * Time.fixedDeltaTime;
         Debug.Log($"{_currentDistance} / {MaxDistance}");
 
         if (_currentDistance >= MaxDistance)
         {
-            LevelManager.Instance.GotoLevel(GameWinScene);
+            OnWin();
+            _isEndFlag = true;
         }
+    }
+
+    private void OnLose()
+    {
+        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            LevelManager.Instance.PlayerDead(player.GetComponent<Character>());
+        }
+    }
+
+    private void OnWin()
+    {
+        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            var health = player.GetComponent<Health>();
+            health?.DamageDisabled();
+        }
+
+        foreach (var engine in Engines)
+        {
+            engine.DamageDisabled();
+        }
+
+        foreach (var enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            var health = enemy.GetComponent<Health>();
+            health.Damage(health.MaximumHealth, gameObject, 0, 0, Vector3.up);
+        }
+
+        LevelManager.Instance.GotoLevel(GameWinScene);
     }
 }
